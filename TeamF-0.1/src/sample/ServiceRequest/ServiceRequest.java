22578451087c7bf2ab066dbbe353db8a5480f874
Prometheus_wy <williswang514@gmail.com>
2017-11-10 15:31:02 -0500

    Service Request Folder
    Includes all the files

---
+++ TeamF-0.1/src/sample/ServiceRequest/ServiceRequest.java
@@ -0,0 +1,54 @@
+package sample.ServiceRequest;
+
+import javax.xml.soap.Node;
+
+//Talal and willis
+public abstract class ServiceRequest {
+
+    protected Node destination;
+    protected String description;
+    protected int serviceID;
+    protected int serviceTime;
+    protected int serviceEmployeeID;
+    protected String typeOfRequest;
+
+    public ServiceRequest(Node destination, String description, int serviceID, int serviceTime, int serviceEmployeeID, String typeOfRequest) {
+        this.destination = destination;
+        this.description = description;
+        this.serviceID = serviceID;
+        this.serviceTime = serviceTime;
+        this.serviceEmployeeID = serviceEmployeeID;
+        this.typeOfRequest = typeOfRequest;
+    }
+
+    public void setServiceEmployeeID(int a) {
+        return;
+    }
+    public Node getDestination() {
+        return this.destination;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getServiceID() {
+        return this.serviceID;
+    }
+    public int getServiceTime() {
+        return this.serviceTime;
+    }
+    public int getServiceEmployeeID() {
+        return this.serviceEmployeeID;
+    }
+    public String getTypeInt() {
+        return this.typeOfRequest;
+    }
+
+
+
+
+
+}
+
+
+
---
+++ TeamF-0.1/src/sample/ServiceRequest/ServiceRequestList.java
@@ -0,0 +1,16 @@
+//package sample;
+//
+//import java.util.PriorityQueue;
+//
+//public class ServiceRequestList {
+//    private PriorityQueue<ServiceRequest> RequestList;
+//
+//    public ServiceRequestList(PriorityQueue<ServiceRequest> requestList) {
+//        RequestList = requestList;
+//    }
+//
+//    public void addRequest(ServiceRequest) {
+//        return
+//
+//    }
+//}
