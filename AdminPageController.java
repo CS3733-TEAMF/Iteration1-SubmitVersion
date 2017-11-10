1700fa7334b2c3a4e7eb79857608fcc50211e5f0
TalalJ <33098774+TalalJ@users.noreply.github.com>
2017-11-10 16:30:35 -0500

    Add files via upload

---
+++ AdminPageController.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class AdminPageController {
+    @FXML
+    public void logout(){
+        Main.startScreen();
+    }
+
+    @FXML
+    public void edit(){
+
+    }
+    @FXML
+    public void ServiceRequest() {}
+
+    @FXML
+    public void addUser(){
+
+    }
+
+    @FXML
+    public void removeUser(){
+
+    }
+
+    @FXML
+    public void settings(){
+
+    }
+}
