5f20a16721d7c8683b7524fe643c655337970106
TalalJ <33098774+TalalJ@users.noreply.github.com>
2017-11-12 12:54:59 -0500

    Add files via upload
    added code to the buttons in the ui

--- TeamF-0.1/src/sample/serviceRequestController.java
+++ TeamF-0.1/src/sample/serviceRequestController.java
@@ -5,10 +5,126 @@ import javafx.scene.control.*;
 
 public class serviceRequestController {
 
+    //top menu bar
     @FXML
     public void backToStart() { Main.startScreen();}
 
     @FXML
     public void backToAdmin() {Main.adminScreen();}
+
+
+    //shared between all service requests
+    @FXML
+    private Label serviceID;
+
+
+    //assistance requests
+    @FXML
+    private TextField assistanceTime;
+
+    @FXML
+    private TextField assistanceUrgency;
+
+    @FXML
+    private TextArea assistanceDescription;
+
+    @FXML
+    public void assistanceThisLocation() {}
+
+    @FXML
+    public void assistanceChooseLocation() {}
+
+    @FXML
+    public void assistanceSendRequest() {}
+
+
+    //food requests
+    @FXML
+    private TextField foodTime;
+
+    @FXML
+    private TextField foodPatient;
+
+    @FXML
+    private TextField foodServingTime;
+
+    @FXML
+    private TextField foodOrder;
+
+    @FXML
+    private TextArea foodDescription;
+
+    @FXML
+    public void foodThisLocation() {}
+
+    @FXML
+    public void foodChooseLocation() {}
+
+    @FXML
+    public void foodSendRequest() {}
+
+
+    //transport requests
+    @FXML
+    private TextField transportTime;
+
+    @FXML
+    private TextField transportPatient;
+
+    @FXML
+    private TextField transportType;
+
+    @FXML
+    private TextArea transportDescription;
+
+    @FXML
+    public void transportThisLocation() {}
+
+    @FXML
+    public void transportChooseLocation() {}
+
+    @FXML
+    public void transportSendRequest() {}
+
+
+    //cleaning requests
+    @FXML
+    private TextField cleanTime;
+
+    @FXML
+    private TextField cleanLevel;
+
+    @FXML
+    private TextArea cleanDescription;
+
+    @FXML
+    public void cleanThisLocation() {}
+
+    @FXML
+    public void cleanChooseLocation() {}
+
+    @FXML
+    public void cleanSendRequest() {}
+
+
+    //security requests
+    @FXML
+    private TextField securityTime;
+
+    @FXML
+    private TextField securityLevel;
+
+    @FXML
+    private TextArea securityDescription;
+
+    @FXML
+    public void securityThisLocation() {}
+
+    @FXML
+    public void securityChooseLocation() {}
+
+    @FXML
+    public void securitySendRequest() {}
+
 
 }
