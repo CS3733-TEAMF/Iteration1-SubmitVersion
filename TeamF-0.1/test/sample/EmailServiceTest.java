fe1f8382b9ff59bb80d3a02d0065810dc79bc45c
Stephanie Racca <srracca@wpi.edu>
2017-11-14 18:56:06 -0500

    Trying to update w tests

--- TeamF-0.1/test/sample/EmailServiceTest.java
+++ TeamF-0.1/test/sample/EmailServiceTest.java
@@ -1,48 +1,84 @@
 // Created by Stephanie and Floris
 // Purpose: To test directions were sent to the user defined email
-package sample;
-import org.junit.Ignore;
+/*Satisfies Integration testing of Email - Steph*/
+/*ALL PASS - Steph*/
 
-import static org.junit.Assert.*;
+package sample;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.util.Vector;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class EmailServiceTest {
 
-//Uses a correct string of directions to send emails out
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
+    //setting up nodes and directions, never changes
 
+    String directions;
 
+    @Before
+    public void directionsSetUp() throws Exception{
+        //Creates a vector with only one node
+        Vector<Node> vector = new Vector<Node>(10);
+        Node n1 = new Node("FDEPT00101", 1614, 829, 1, "Tower", "DEPT", "Center for International Medecine", "CIM", 'F');
+        vector.add(n1);
+        Node n2 = new Node("FHALL00201", 1640, 850, 1, "Tower", "HALL", "Chapel Hall Point 1", "CHP1", 'F');
+        vector.addElement(n2);
+        //Creates string of directions
+        directions = NavigationPageController.directions(vector);
+    }
     //Send Email Tests
 
     //Steph - Uses an invalid email to verify that the error has been properly handled by the system
-    @Ignore@org.junit.Test
+    @org.junit.Test
     public void sendNotValidEmail() throws Exception {
+        //tests validity
+        thrown.expect(NullPointerException.class);
         EmailService notValid = new EmailService("teamFCS3733@gmail.com", "FuschiaFairiesSoftEng");
-        //check that status is correctly set
-        assertEquals("Setting up email services...", notValid.status);
-        //uses a non-valid email
-        notValid.sendEmail( "Directions","teamZCS3733@gmail.com");
-        //if a vaild email is passed, the status should be set to the string below
-        assertEquals("That is not a valid Email, try again!", notValid.status);
+        notValid.sendEmail(directions, "notValid");
     }
 
     //Steph - checks if email was successfully sent with valid email
     @org.junit.Test
     public void sendSuccessfulEmail() throws Exception {
+        //Creates a vector with only one node
+        Vector<Node> vector = new Vector<Node>(10);
+        Node n1 = new Node("FDEPT00101", 1614, 829, 1, "Tower", "DEPT", "Center for International Medecine", "CIM", 'F');
+        vector.add(n1);
+        Node n2 = new Node("FHALL00201", 1640, 850, 1, "Tower", "HALL", "Chapel Hall Point 1", "CHP1", 'F');
+        vector.addElement(n2);
+        //Creates string of directions
+        String directions = NavigationPageController.directions(vector);
+
+        //tests validity
         EmailService successful = new EmailService("teamFCS3733@gmail.com", "FuschiaFairiesSoftEng");
-        String validDirections = "";
         //if successful, the status should be "sent message"
-        successful.sendEmail(validDirections, "teamFCS3733@gmail.com");
+        successful.sendEmail(directions, "teamFCS3733@gmail.com");
         assertEquals("Sent message", successful.status);
     }
 
     //Steph - checks if status was properly set for an error by using incorrect username
-    @org.junit.Test
+    @org.junit.Test(expected = Exception.class)
     public void sendErrorEmail() throws Exception {
+        //Creates a vector with only one node
+        Vector<Node> vector = new Vector<Node>(10);
+        Node n1 = new Node("FDEPT00101", 1614, 829, 1, "Tower", "DEPT", "Center for International Medecine", "CIM", 'F');
+        vector.add(n1);
+        Node n2 = new Node("FHALL00201", 1640, 850, 1, "Tower", "HALL", "Chapel Hall Point 1", "CHP1", 'F');
+        vector.addElement(n2);
+        //Creates string of directions
+        String directions = NavigationPageController.directions(vector);
+
+        //tests validity
         //uses incorrect email address
         EmailService error = new EmailService("teamZCS3733@gmail.com", "FuschiaFairiesSoftEng");
-        String validDirections = "";
-        error.sendEmail(validDirections, "");
+        error.sendEmail(directions, "TeamFCS3733");
         //if an error occurs, such as setting up the server, the status should be set to this
         assertEquals("An error occurred while sending the message", error.status);
     }
---
+++ TeamF-0.1/test/sample/PathDrawITest.java
@@ -0,0 +1,8 @@
+/*Steph - Path Drawing Tests
+* Purpose: See how pathfinding affects pathdrawing*/
+/*UNFINISHED - Steph*/
+
+package sample;
+
+public class PathDrawITest {
+}
